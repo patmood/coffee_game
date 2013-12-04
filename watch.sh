dcb8a8d9b1f6ebbd47cd55e488da8a30467dde08
patmood <patrick.n.moody@gmail.com>
2013-12-04 13:05:02 +1030

    watch for new level file

--- watch.sh
+++ watch.sh
@@ -4,6 +4,7 @@ xargs coffee -w -b -j script/main.js \
              -c << EOF
                 src/gfx.coffee
                 src/keys.coffee
+                src/levels/Level.coffee
                 src/entities/_Entity.coffee
                 src/entities/Player.coffee
                 src/entities/Ninja.coffee
