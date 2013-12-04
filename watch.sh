79481829313abd172b01f795cfc124679c4fcbbd
patmood <patrick.n.moody@gmail.com>
2013-12-04 13:21:52 +1030

    update watch directories

--- watch.sh
+++ watch.sh
@@ -4,8 +4,10 @@ xargs coffee -w -b -j script/main.js \
              -c << EOF
                 src/gfx.coffee
                 src/keys.coffee
+                src/levels/levels.coffee
                 src/levels/Level.coffee
                 src/blocks/Block.coffee
+                src/blocks/Dirt.coffee
                 src/entities/_Entity.coffee
                 src/entities/Player.coffee
                 src/entities/Ninja.coffee
